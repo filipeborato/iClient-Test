c4bc1802d6692cb3550e69f14a4f41afadb1adc2
filipeborato <filipeborato@hotmail.com>
2022-01-17 09:20:47 -0300

    Criando a model Prescriptions

--- prescriptions/models.py
+++ prescriptions/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Prescription(models.Model):
+    clinic_id = models.IntegerField()
+    clinic_name = models.CharField(max_length=60)
+    physician_id = models.IntegerField()
+    physician_name = models.CharField(max_length=100)
+    physician_crm = models.CharField(max_length=15)
+    patient_id = models.IntegerField()
+    patient_name = models.CharField(max_length=60)
+    patient_email = models.CharField(max_length=40)
+    patient_phone = models.CharField(max_length=40)
+    prescription_id = models.IntegerField()
